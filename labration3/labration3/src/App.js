217f33161f5b24cfada4f765914794e453750bd1
Amir <amir.moghadasian58@gmail.com>
2022-06-10 11:05:35 +0200

    change style

--- labration3/labration3/src/App.js
+++ labration3/labration3/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <div>
       <Navbar />
      
-      <div>
+      <div className="container">
         
           <Routes>
             <Route path='/' element={<Home />} />
